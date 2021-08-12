f2beeeb14648b214f4a2319c66bc6fed8679568b
Mark AI <chanthavong_12@hotmail.com>
2021-08-12 20:22:47 +0700

    init

---
+++ login.js
@@ -0,0 +1,24 @@
+let users = [
+    { username: 'mark', password: '1234' },
+    { username: 'alex', password: '1234' },
+    { username: 'jonh', password: '1234' },
+    { username: 'jen', password: '1234' }
+]
+function _id(id) {
+    return document.getElementById(id)
+}
+function signIn() {
+    let username = _id('txtuser').value
+    let password = _id('txtpass').value
+    console.log(username)
+    console.log(password)
+    let user = users.filter(
+        (user) => (user.username == username && user.password == password)
+    )
+    console.log(user)
+    if (user.length>0) {
+        alert('success')
+    } else {
+        alert('sign in fail!')
+    }
+}
